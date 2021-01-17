e137f54e5966d8b09f1b1fb3e629a6ce997d5069
tiagosantos-dev <tiagoltj@hotmail.com>
2021-01-17 18:27:37 -0300

    Testando componente

--- src/app/components/card/card.component.spec.ts
+++ src/app/components/card/card.component.spec.ts
@@ -2,6 +2,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+
 import { Observable, of } from 'rxjs';
 import { Client } from 'src/app/models/client.model';
 import { ClientService } from 'src/app/services/client.service';
@@ -11,8 +12,9 @@ import { CardComponent } from './card.component';
 describe('CardComponent', () => {
   let component: CardComponent;
   let fixture: ComponentFixture<CardComponent>;
-  
-
+  let clientService :ClientService;
+  let clienteServiceMock;
+  let listClient : Client[] = []
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [CardComponent],
@@ -24,63 +26,15 @@ describe('CardComponent', () => {
 
     fixture = TestBed.createComponent(CardComponent);
     component = fixture.componentInstance;
+    
     fixture.detectChanges();
-    let listClient = [{
-      "id": 1,
-      "nome": "Daniel Tomás Almeida",
-      "idade": 63,
-      "cpf": "766.211.857-66",
-      "rg": "23.774.287-1",
-      "data_nasc": "18\/06\/1958",
-      "sexo": "Masculino",
-      "signo": "Gêmeos",
-      "mae": "Jéssica Flávia Lara",
-      "pai": "Theo Marcos Vinicius Francisco Almeida",
-      "email": "danieltomasalmeida..danieltomasalmeida@suzano.com.br",
-      "senha": "LO7ydJ6Upo",
-      "cep": "79082-076",
-      "endereco": "Travessa Ipeúna",
-      "numero": 544,
-      "bairro": "Guanandi II",
-      "cidade": "Campo Grande",
-      "estado": "MS",
-      "telefone_fixo": "(67) 3827-3988",
-      "celular": "(67) 98933-6753",
-      "altura": "1,88",
-      "peso": 59,
-      "tipo_sanguineo": "B+",
-      "cor": "vermelho"
-    }, {
-      "id": 2,
-      "nome": "Gabriela Elaine Nicole Galvão",
-      "idade": 37,
-      "cpf": "494.808.190-66",
-      "rg": "13.614.477-9",
-      "data_nasc": "26\/04\/1984",
-      "sexo": "Feminino",
-      "signo": "Touro",
-      "mae": "Raimunda Rafaela",
-      "pai": "Calebe Mário Samuel Galvão",
-      "email": "ggabrielaelainenicolegalvao@hersa.com.br",
-      "senha": "E3nitqb1pc",
-      "cep": "76875-760",
-      "endereco": "Rua São Felipe",
-      "numero": 905,
-      "bairro": "Coqueiral",
-      "cidade": "Ariquemes",
-      "estado": "RO",
-      "telefone_fixo": "(69) 3603-0532",
-      "celular": "(69) 99334-5236",
-      "altura": "1,53",
-      "peso": 88,
-      "tipo_sanguineo": "AB-",
-      "cor": "vermelho"
-    },];
+    
 
 
-    let clientService = TestBed.inject(ClientService);
-    spyOn(clientService, 'getAll').and.returnValue(of(listClient));
-  
+    clientService = TestBed.inject(ClientService);
+    let functions = spyOn(clientService, 'getAll').and.returnValue(of(listClient));
+    clientService.getAll();
+
   });
 
 
@@ -88,7 +42,7 @@ describe('CardComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should title h2',()=>{
+  it('should title h3',()=>{
   
 
     // const de : DebugElement = fixture.debugElement;
@@ -97,6 +51,30 @@ describe('CardComponent', () => {
     const cardElement :HTMLElement = fixture.nativeElement
     const title = cardElement.querySelector('h3');
     expect(title?.textContent).toContain('Cliente:')
+
+
+  })
+
+  it('should verficate if contains li', () => {
+
+
+    const cardElement : HTMLElement = fixture.nativeElement;
+    const lis = cardElement.querySelectorAll('ul li')
+
+    expect(lis.length).toEqual(10);
+    
+  })
+
+  it('should vericate if contains two card',()=>{
+  
+
+    fixture.detectChanges();
+    const cardElement : HTMLElement = fixture.nativeElement;
+    const cards = cardElement.querySelectorAll('.card');
+
+ 
+    expect(cards.length).toEqual(1);
+
 
   })
 
--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,16 +1,28 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Observable, of } from 'rxjs';
+import { Client } from 'src/app/models/client.model';
+import { ClientService } from 'src/app/services/client.service';
 
 import { HomeComponent } from './home.component';
+import { ClientStub } from './shared/client.stub';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let clientService :ClientService;
 
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ HomeComponent ],
-      imports:[HttpClientTestingModule]
+      imports:[HttpClientTestingModule],
+      providers:[
+        {provide: ClientService, useClass: ClientStub}
+
+      ]
     })
     .compileComponents();
   });
@@ -19,9 +31,22 @@ describe('HomeComponent', () => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should if exist two card', () =>{
+
+    const homeElement : HTMLElement = fixture.nativeElement;
+    const de = fixture.debugElement
+  
+    const cards = homeElement.querySelectorAll('.card');
+    expect(cards.length).toEqual(2)
+    debugger;
+
+  })
+
 });
